1023756447a6eb1148788155c1890f2f14fac5c8
Pedro Nieto <pedronieto.2005@gmail.com>
2023-03-28 14:49:21 -0300

    Create index.js

---
+++ Level/index.js
@@ -0,0 +1,11 @@
+basic.forever(function (){
+    let x = input.acceleration(Dimension.X)
+    let y = input.acceleration(Dimension.Y)
+    if(Math.abs(x) > 32) {
+        basic.showIcon(IconNames.Sad)
+    } else if (Math.abs(y) > 32){
+        basic.showIcon(IconNames.Angry)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
+})
