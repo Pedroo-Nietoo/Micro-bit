c46cc9209b428e67e687eace92872c433c67dc4b
Pedro Nieto <pedronieto.2005@gmail.com>
2023-03-28 16:17:16 -0300

    Create index.js

---
+++ Tools/Stopwatch/index.js
@@ -0,0 +1,9 @@
+let start = 0
+input.onButtonPressed(Button.A, function() {
+    start = input.runningTime()
+})
+
+input.onButtonPressed(Button.B, function () {
+    let elapsed = input.runningTime() - start
+    basic.showNumber(Math.idiv(elapsed, 1000))
+})
