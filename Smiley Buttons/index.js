ec753cc6039e5bf90f7cf6e3d4dfaf244e88a91e
Pedro Nieto <pedronieto.2005@gmail.com>
2023-03-28 14:26:09 -0300

    Create index.js

---
+++ Smiley Buttons/index.js
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Silly)
+    basic.showIcon(IconNames.Surprised)
+})
